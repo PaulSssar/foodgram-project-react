9224ca23c6b42127a35045a2d29075aabcb88777
Павел Сарыгин <paulssssar@gmail.com>
2023-06-02 09:39:21 +0700

    serializer recipes to_repredentation

--- backend/api/views.py
+++ backend/api/views.py
@@ -85,7 +85,7 @@ class RecipeViewSet(viewsets.ModelViewSet,
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
     http_method_names = [
-        m for m in viewsets.ModelViewSet.http_method_names if m not in ['put']
+        method for method in viewsets.ModelViewSet.http_method_names if method not in ['put']
     ]
 
     def get_serializer_class(self):
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -18,6 +18,7 @@ class Ingredient(models.Model):
     class Meta:
         verbose_name = 'Ингридиент'
         verbose_name_plural = 'Ингридиенты'
+        ordering =('pk',)
 
     def __str__(self):
         return self.name
@@ -89,10 +90,17 @@ class Recipes(models.Model):
             ),
         ]
     )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        help_text='Дата публикации',
+        null=True
+    )
 
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
+        ordering = ('-pub_date',)
 
     def __str__(self):
         return self.name
