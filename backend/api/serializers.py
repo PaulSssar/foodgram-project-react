b2ce1a8f6d0a5b0cd3764c9338e6ae6011fa75bf
Павел Сарыгин <paulssssar@gmail.com>
2023-06-06 09:03:05 +0700

    serializer recipes to_repredentation

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -14,17 +14,17 @@ class MyUserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'email', 'username', 'first_name',
                   'last_name', 'is_subscribed')
-
-    def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+    #
+    # def create(self, validated_data):
+    #     user = User(
+    #         email=validated_data['email'],
+    #         username=validated_data['username'],
+    #         first_name=validated_data['first_name'],
+    #         last_name=validated_data['last_name'],
+    #     )
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #     return user
 
     def get_is_subscribed(self, obj):
         user = self.context.get('request').user
