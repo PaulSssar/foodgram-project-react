5227b4d13d17a1a916509c30573dda65659b7cda
Павел Сарыгин <paulssssar@gmail.com>
2023-06-07 19:12:19 +0700

    userserializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -28,7 +28,7 @@ class MyUserSerializer(UserSerializer):
         return user
 
     def get_is_subscribed(self, obj):
-        user = self.context.get['request'].user
+        user = self.context.get('request')
         if user.is_anonymous:
             return False
         return Follow.objects.filter(user=user, author=obj).exists()
