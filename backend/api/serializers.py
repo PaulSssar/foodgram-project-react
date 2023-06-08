56bf56f3d5943c5e348eabb19cc79c82ba888e0f
Павел Сарыгин <paulssssar@gmail.com>
2023-06-08 12:16:56 +0700

    useserializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,7 +9,7 @@ from users.models import Follow, User
 
 
 class MyUserSerializer(UserSerializer):
-    is_subscribed = serializers.SerializerMethodField(read_only=True)
+    # is_subscribed = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
@@ -27,11 +27,11 @@ class MyUserSerializer(UserSerializer):
         user.save()
         return user
 
-    def get_is_subscribed(self, obj):
-        user = self.context.get('request').user
-        if user.is_anonymous:
-            return False
-        return Follow.objects.filter(user=user, author=obj).exists()
+    # def get_is_subscribed(self, obj):
+    #     user = self.context.get('request').user
+    #     if user.is_anonymous:
+    #         return False
+    #     return Follow.objects.filter(user=user, author=obj).exists()
 
 
 class FollowSerializer(MyUserSerializer):
